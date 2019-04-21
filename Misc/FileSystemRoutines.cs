7d78ecd0fa90c18e03d0c353d18a16e68dec1159
Sergey Stoyan <sergey.stoyan@gmail.com>
2019-04-21 15:26:33 +0300

    v3.4

--- Misc/FileSystemRoutines.cs
+++ Misc/FileSystemRoutines.cs
@@ -75,15 +75,44 @@ namespace Cliver
             Directory.Delete(directory, false);
         }
 
+        public static bool DeleteDirectorySteadfastly(string directory, bool recursive = true)
+        {
+            bool error = false;
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                try
+                {
+                    File.SetAttributes(file, FileAttributes.Normal);
+                    File.Delete(file);
+                }
+                catch
+                {
+                    error = true;
+                }
+            }
+            if (recursive)
+                foreach (string d in Directory.GetDirectories(directory))
+                    DeleteDirectorySteadfastly(d, recursive);
+            try
+            {
+                Directory.Delete(directory, false);
+            }
+            catch
+            {
+                error = true;
+            }
+            return !error;
+        }
+
         public static void CopyFile(string file1, string file2, bool overwrite = false)
         {
-            CreateDirectory(PathRoutines.GetDirFromPath(file2), false);
+            CreateDirectory(PathRoutines.GetFileDir(file2), false);
             File.Copy(file1, file2, overwrite);
         }
 
         public static void MoveFile(string file1, string file2, bool overwrite = true)
         {
-            CreateDirectory(PathRoutines.GetDirFromPath(file2), false);
+            CreateDirectory(PathRoutines.GetFileDir(file2), false);
             if (File.Exists(file2))
             {
                 if (!overwrite)
