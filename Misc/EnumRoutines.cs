f7a0c70d7d85d533f200119a708e27ba04f2f621
Sergey Stoyan <s.y.stoyan@gmail.com>
2023-12-25 11:59:27 +0200

    6.42.0

---
+++ Misc/EnumRoutines.cs
@@ -0,0 +1,22 @@
+﻿//********************************************************************************************
+//Author: Sergiy Stoyan
+//        s.y.stoyan@gmail.com, sergiy.stoyan@outlook.com, stoyan@cliversoft.com
+//        http://www.cliversoft.com
+//********************************************************************************************
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+
+namespace Cliver
+{
+    public static class EnumRoutines
+    {
+        public static IEnumerable<T> GetValues<T>() where T : Enum
+        {
+            foreach (T v in Enum.GetValues(typeof(T)))
+                yield return v;
+        }
+    }
+}
+
