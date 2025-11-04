6f774bb35429072b0238f15974cc02edcd77bc2a
Sergiy Stoyan <s.y.stoyan@gmail.com>
2025-11-04 14:19:34 +0200

    6.54.0
    - GetUrlQuery() enhanced;

--- Misc/WebRoutines.cs
+++ Misc/WebRoutines.cs
@@ -24,6 +24,16 @@ namespace Cliver
             return string.Join("&", names2value.Select(n2v => WebUtility.UrlEncode(n2v.Name) + "=" + WebUtility.UrlEncode(n2v.Value.ToString())));
         }
 
+        static public string GetUrlQuery(Dictionary<string, string> names2value)
+        {
+            return GetUrlQuery(names2value.Select(n2v => (n2v.Key, n2v.Value)));
+        }
+
+        static public string GetUrlQuery(IEnumerable<(string Name, string Value)> names2value)
+        {
+            return string.Join("&", names2value.Select(n2v => WebUtility.UrlEncode(n2v.Name) + "=" + WebUtility.UrlEncode(n2v.Value)));
+        }
+
         static public string GetUrlEncoded(string value)
         {
             return WebUtility.UrlEncode(value);
